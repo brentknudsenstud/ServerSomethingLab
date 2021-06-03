785b5f42d0ba7d6ed540b2a1207b49d39ed38f88
Brent Knudsen <brent@brents-mbp.lan>
2021-06-02 18:09:36 -0600

    Last commit, I think

--- client.js
+++ client.js
@@ -3,3 +3,10 @@ const net = require('net');
 const socket = net.createConnection({port: 1974}, () => {
     console.log('connected to server');
 });
+socket.setEncoding('utf-8');
+process.stdin.pipe(socket);
+
+// listen to the data event
+socket.on('data', data => {
+    console.log(data);
+})
--- server.js
+++ server.js
@@ -1,10 +1,48 @@
+
 const net = require('net');
+const fs = require('fs');
+let clients = [];
+let count = 0;
+
+function writeToChatLog(data) {
+    fs.appendFileSync('chat.log', '\n' + data, )
+}
+
+function getOtherClients(socket) {
+    return clients.filter(socketItem => {
+        return socketItem !== socket
+    })
+}
+
+function sendToOtherClients(data, socket) {
+    const otherClients = getOtherClients(socket);
+    otherClients.forEach(client => client.write(socket.name + ' ' 
+    + data))
+}
 
 const server = net.createServer((socket) => {
-    socket.name = 'myNewClient'; 
+    clients.push(socket);
+    console.log('Number of clients, ', clients.length)
+    count = count + 1;
+    socket.name = 'myNewClient, ' + count ; 
     console.log(`socket address: ${socket.remoteAddress}`);
     console.log(`socket port: ${socket.remotePort}`);
     socket.write('Welcome to the chat server!'); 
+    socket.setEncoding('utf-8');
+
+    socket.on('data', data => {
+        writeToChatLog(data)
+        sendToOtherClients(data, socket)
+        console.log(data);
+    })
+    socket.on('end', function () {
+        const disconnectMessage = 'disconnecting ' + socket.name;
+
+        sendToOtherClients(disconnectMessage, socket);
+        clients = getOtherClients(socket);
+        writeToChatLog('disconnecting ' + socket.name)
+        console.log('Number of clients, ', clients.length)
+    })
 }).listen(1974);
 
 console.log('Connection made! Listening on port 1974.');
